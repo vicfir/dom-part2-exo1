28f6c822a89af3c601c7142a028788208b7c0841
vic fir <vicfir@hotmail.fr>
2023-01-13 09:44:48 +0100

    fin

---
+++ main.js
@@ -0,0 +1,6 @@
+let tartine = document.getElementsByClassName("tartine");
+console.log(tartine);
+let toast = document.getElementById("toast");
+console.log(toast);
+let tartineLast =  document.getElementsByClassName("tartine")[1];
+console.log(tartineLast);
